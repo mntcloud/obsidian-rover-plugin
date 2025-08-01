d84d039fc5af833befa9298620ed91f61fd5939c
mntcloud <40216914+mntcloud@users.noreply.github.com>
2025-08-01 12:57:41 +0300

    feat: add moving files to the root

--- src/view/components/ExplorerView.tsx
+++ src/view/components/ExplorerView.tsx
@@ -7,10 +7,13 @@ import { Folder } from "./FileManager/Folder";
 import { RoverFile } from "view/models/data/Base";
 import { Obsidian } from "view/models/data/Obsidian";
 import { Explorer } from "view/models/ExplorerModel";
+import { TAbstractFile } from "obsidian";
 
 export class ExplorerView implements m.ClassComponent {
     root: RoverFile[];
     dom: Element;
+
+    highlight = false;
 
     oninit(vnode: m.Vnode<Attr, this>) {
         this.root = [];
@@ -28,9 +31,51 @@ export class ExplorerView implements m.ClassComponent {
             if (element) {
                 await new Promise((resolve) => {
                     element.dispatchEvent(
-                        new CustomEvent("createdelete", { detail: {complete: resolve}, bubbles: false }),
+                        new CustomEvent("createdelete", {
+                            detail: { complete: resolve },
+                            bubbles: false,
+                        }),
                     );
-                }) 
+                });
+            }
+        }
+    }
+
+    onDragEnterExit() {
+        this.highlight = !this.highlight;
+    }
+
+    onDragEnd() {
+        this.highlight = false;
+    }
+
+    async onDrop(ev: DragEvent) {
+        ev.preventDefault();
+        ev.stopPropagation();
+
+        if (ev.dataTransfer && ev.dataTransfer.items.length == 1) {
+            let file: TAbstractFile | null = null;            
+
+            switch (ev.dataTransfer.items[0].type) {
+                case "application/rover.file": {
+                    const path = ev.dataTransfer!.getData(
+                        "application/rover.file",
+                    );
+
+                    file = Obsidian!.vault.getFileByPath(path)
+                    break;
+                }
+                case "application/rover.folder": {
+                    const path = ev.dataTransfer!.getData(
+                        "application/rover.folder",
+                    );
+
+                    file = Obsidian!.vault.getFolderByPath(path)
+                }
+            }
+
+            if (file) {
+                await Obsidian!.app.fileManager.renameFile(file, file.name)
             }
         }
     }
@@ -67,7 +112,16 @@ export class ExplorerView implements m.ClassComponent {
 
     view(vnode: m.Vnode<Attr, this>) {
         return (
-            <div className="rover-container rover-file-manager">
+            <div
+                className={`rover-container rover-file-manager ${
+                    this.highlight ? "highlight" : ""
+                }`}
+                ondragenter={() => this.onDragEnterExit()}
+                ondragleave={() => this.onDragEnterExit()}
+                ondragend={() => this.onDragEnd()}
+                ondragover={(ev: DragEvent) => ev.preventDefault()}
+                ondrop={async (ev: DragEvent) => await this.onDrop(ev)}
+            >
                 {this.root.length
                     ? this.root.map((file) => {
                         return file.isFolder
