1c5e779446971f16ca9cd47bd7d5d99be3759182
mntcloud <40216914+mntcloud@users.noreply.github.com>
2026-05-08 20:36:21 +0300

    Fix Obsidian API wrapper object

--- src/view/models/Obsidian.ts
+++ src/view/models/Obsidian.ts
@@ -1,19 +1,34 @@
 import { App } from "obsidian";
 import { RoverPluginSettings, ObsidianAppModel } from "../../core";
 
+let backingValue: ObsidianAppModel | undefined = undefined;
+
 export function init(
   app: App,
   settings: RoverPluginSettings,
   save: () => void,
 ) {
-  Obsidian = {
+  backingValue = {
     app: app, // for modals
-    workspace: app.workspace,
     vault: app.vault,
+    workspace: app.workspace,
 
     settings: settings,
     save: save,
   };
 }
 
-export let Obsidian: ObsidianAppModel | undefined;
+// We need a reliable reference
+// to pass as a models constructor argument at models import
+export let Obsidian = new Proxy(
+  {},
+  {
+    get(target, prop, receiver) {
+      if (backingValue) {
+        return Reflect.get(backingValue, prop, receiver);
+      }
+
+      console.log("fuck, doesn't work...");
+    },
+  },
+) as ObsidianAppModel;
--- src/view/models/index.ts
+++ src/view/models/index.ts
@@ -1,15 +1,10 @@
 // this file is full of sideffects
 
-import { ModifyItemModal } from "rover/modals/bookmarks/ModifyItem";
 import { BookmarksBaseModel } from "./BookmarksModel";
 import { Obsidian } from "./Obsidian";
 import { ExplorerBaseModel } from "./ExplorerModel";
 import { RecentsBaseModel } from "./RecentsModel";
-import { CreateItemModal } from "rover/modals/bookmarks/CreateItem";
-import { CreateFolderModal } from "rover/modals/bookmarks/CreateFolder";
 
 export const Bookmarks = new BookmarksBaseModel(Obsidian);
-
 export const Recents = new RecentsBaseModel(Obsidian);
-
 export const Explorer = new ExplorerBaseModel(Obsidian, Recents);
